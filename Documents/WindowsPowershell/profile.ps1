2a5764420963907830afd7904074295e91a3da8b
John Weldon <johnweldon4@gmail.com>
2018-08-14 22:56:16 -0700

    Version Check

--- Documents/WindowsPowershell/profile.ps1
+++ Documents/WindowsPowershell/profile.ps1
@@ -1,4 +1,6 @@
 
 $env:PATH += "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 
-Set-PSReadlineOption -Editmode vi
+if ( $PSVersionTable.PSVersion.Major -ge 6 ) {
+	Set-PSReadlineOption -Editmode vi
+}
